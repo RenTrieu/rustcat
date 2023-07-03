957738713b2d179f5b2e0c0ed8fa379f0b63907a
Ren Trieu <darrentrieunguyen@gmail.com>
2023-07-02 19:01:48 -0700

    Added setup and debug code for getopts cli options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use std::fs;
 use std::path::Path;
 use std::io::Error;
+use std::env;
+extern crate getopts;
+use getopts::Options;
 
 fn main() {
+  let args: Vec<String> = env::args().collect();
+  let program = args[0].clone();
+
+  let mut opts = Options::new();
+  let matches = match opts.parse(&args[1..]) {
+      Ok(m) => { m }
+      Err(f) => { panic!("{}", f.to_string()) }
+  };
+
+  // TODO: Remove the following debug loop
+  for (i, arg) in args.iter().enumerate() {
+    println!("i: {}, arg: {}", i, arg);
+  }
   println!("Hello, world!");
 }
 
+/// Reads a file from a given path into a buffer
+/// Returns the buffer
 fn read_file(f_path: &Path) -> Result<Vec<u8>, Error> {
   let f_buf = fs::read(f_path);
   f_buf

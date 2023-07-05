8dbefc387552879f2341ffd37c4124a0015430c0
Ren Trieu <darrentrieunguyen@gmail.com>
2023-07-04 19:33:36 -0700

    Removing getopts

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 use std::fs;
 use std::path::Path;
-use std::io::Error;
+use std::io::{self, Error, Write};
 use std::env;
-extern crate getopts;
-use getopts::Options;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
   let program = args[0].clone();
 
-  let mut opts = Options::new();
-  let matches = match opts.parse(&args[1..]) {
-      Ok(m) => { m }
-      Err(f) => { panic!("{}", f.to_string()) }
-  };
+  if args.len() <= 1 {
+    std::process::exit(exitcode::USAGE);
+  }
+
+  // let mut opts = Options::new();
+  // let matches = match opts.parse(&args[1..]) {
+  //     Ok(m) => { m }
+  //     Err(f) => { panic!("{}", f.to_string()) }
+  // };
 
   // TODO: Remove the following debug loop
   for (i, arg) in args.iter().enumerate() {
     println!("i: {}, arg: {}", i, arg);
   }
-  println!("Hello, world!");
+  let f_path = args[1].clone();
+  let f_buf = read_file(Path::new(f_path.as_str())).unwrap();
+  let num_bytes = write_stdout(f_buf).unwrap();
+  println!("num_bytes: {}", num_bytes);
+  println!("Rust Cat Run!");
 }
 
 /// Reads a file from a given path into a buffer
@@ -27,6 +33,13 @@ fn main() {
 fn read_file(f_path: &Path) -> Result<Vec<u8>, Error> {
   let f_buf = fs::read(f_path);
   f_buf
+}
+
+/// Writes out a file buffer to stdout
+fn write_stdout(f_buf: Vec<u8>) -> Result<usize, io::Error> {
+  let mut stdout = io::stdout().lock();
+  let num_bytes = stdout.write(f_buf.as_slice());
+  num_bytes
 }
 
 #[cfg(test)]
@@ -76,5 +89,13 @@ mod tests {
     // Reading from file
     let f_buf = read_file(f_path);
     assert!(f_buf.is_err());
+  }
+
+  #[test]
+  fn write_stdout_small_buffer() {
+    // Creating small Vec<u8> buffer
+    let buf = String::from("Hello, world!").into_bytes();
+    let num_bytes = write_stdout(buf);
+    assert_eq!(num_bytes.unwrap(), 13);
   }
 }

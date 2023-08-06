ca21f69629ef716d8264a7b16b2d721dc82c13b5
Ren Trieu <darrentrieunguyen@gmail.com>
2023-08-05 21:14:43 -0700

    Added initial implementation of a manual alternative to read_file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::path::Path;
-use std::io::{self, Error, Write};
+use std::io::{self, Error, Write, read_to_string};
 use clap::Parser;
 
 #[allow(unused)]
@@ -21,11 +21,32 @@ fn main() {
   println!("Rust Cat Run!");
 }
 
-/// Reads a file from a given path into a buffer
+/// Reads a file from a given path into a buffer using fs::read
 /// Returns the buffer
 fn read_file(f_path: &Path) -> Result<Vec<u8>, Error> {
   let f_buf = fs::read(f_path);
   f_buf
+}
+
+// Reads a file from a given path into a buffer manually
+// Returns the buffer
+fn read_file_manual(f_path: &Path) -> Result<Vec<u8>, Error> {
+  let mut result = Vec::new();
+  let f_result = fs::read_to_string(f_path);
+  let f_string;
+  match f_result {
+    Ok(x) => f_string = x,
+    Err(x) => return Err(x),
+  }
+ 
+  for line in f_string.lines() {
+    for byte in line.as_bytes() {
+      result.push(*byte);
+    }
+    result.push(b'\n');
+  }
+  result.pop();
+  Ok(result)
 }
 
 /// Writes out a file buffer to stdout
@@ -61,6 +82,25 @@ mod tests {
   }
 
   #[test]
+  fn read_file_manual_simple() {
+    // Creating a simple file
+    let f_path = Path::new("./foo_man.txt");
+    let mut f = File::create(f_path).unwrap();
+
+    // Writing to file
+    f.write_all(b"Hello, world!").unwrap();
+
+    // Reading from file
+    let f_buf = read_file_manual(f_path);
+
+    // Removing file
+    remove_file(f_path).unwrap();
+    let expected_vec = String::from("Hello, world!").into_bytes();
+    assert_eq!(f_buf.unwrap(), expected_vec);
+  }
+
+
+  #[test]
   fn read_file_empty() {
     // Creating a simple file
     let f_path = Path::new("./foo2.txt");
@@ -75,12 +115,36 @@ mod tests {
   }
 
   #[test]
+  fn read_file_manual_empty() {
+    // Creating a simple file
+    let f_path = Path::new("./foo_man2.txt");
+    let _f = File::create(f_path).unwrap();
+
+    // Reading from file
+    let f_buf = read_file_manual(f_path);
+
+    // Removing file
+    remove_file(f_path).unwrap();
+    assert_eq!(f_buf.unwrap(), Vec::new());
+  }
+
+  #[test]
   fn read_file_nonexistent() {
     // Creating a simple file
     let f_path = Path::new("./foo3.txt");
 
     // Reading from file
     let f_buf = read_file(f_path);
+    assert!(f_buf.is_err());
+  }
+
+  #[test]
+  fn read_file_manual_nonexistent() {
+    // Creating a simple file
+    let f_path = Path::new("./foo_man3.txt");
+
+    // Reading from file
+    let f_buf = read_file_manual(f_path);
     assert!(f_buf.is_err());
   }
 
@@ -110,4 +174,24 @@ mod tests {
     let expected_vec = test_str.into_bytes();
     assert_eq!(f_buf.unwrap(), expected_vec);
   }
+
+  #[test]
+  fn read_file_manual_with_newline() {
+    // Creating a simple file
+    let f_path = Path::new("./foo_man4.txt");
+    let mut f = File::create(f_path).unwrap();
+
+    // Writing to file
+    let test_str = String::from("Hello, world!\nHow are things going?");
+    f.write_all(test_str.as_bytes()).unwrap();
+
+    // Reading from file
+    let f_buf = read_file_manual(f_path);
+
+    // Removing file
+    remove_file(f_path).unwrap();
+    let expected_vec = test_str.into_bytes();
+    assert_eq!(f_buf.unwrap(), expected_vec);
+  }
+
 }
